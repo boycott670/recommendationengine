a54311d76d84382a333a2d4cbfa74de247b17234
takingsss12 <takingsss12@gmail.com>
2018-06-21 09:16:17 +0100

    similarityUsingEuclideanDistance passed [refactor#1]

--- src/main/java/com/sqli/challenge/similarity/EuclidianSimilarityMetric.java
+++ src/main/java/com/sqli/challenge/similarity/EuclidianSimilarityMetric.java
@@ -1,9 +1,9 @@
 package com.sqli.challenge.similarity;
 
-import com.sqli.challenge.model.Review;
-
 import java.util.ArrayList;
 import java.util.Collection;
+
+import com.sqli.challenge.model.Review;
 
 class EuclidianSimilarityMetric implements SimilarityMetric{
     private static final EuclidianSimilarityMetric instance = new EuclidianSimilarityMetric();
@@ -14,18 +14,34 @@ class EuclidianSimilarityMetric implements SimilarityMetric{
     public static SimilarityMetric getInstance() {
         return instance;
     }
+    
+    private Collection<ReviewPair> sharedReviews(Collection<Review> reviewsFstPerson, Collection<Review> reviewsSndPerson)
+    {
+      Collection<ReviewPair> reviewPairs = new ArrayList<>();
+      
+      firstPersonReviews:
+      for(Review fstReview: reviewsFstPerson){
+        secondPersonReviews:
+          for(Review sndReview: reviewsSndPerson){
+              if(fstReview.hasSameProduct(sndReview)){
+                  reviewPairs.add(new ReviewPair(fstReview, sndReview));
+                  continue firstPersonReviews;
+              }
+          }
+      }
+      
+      return reviewPairs;
+    }
 
     @Override
     public double score(Collection<Review> reviewsFstPerson, Collection<Review> reviewsSndPerson) {
-        Collection<ReviewPair> reviewPairs = new ArrayList<>();
-        for(Review fstReview: reviewsFstPerson){
-            for(Review sndReview: reviewsSndPerson){
-                if(fstReview.hasSameProduct(sndReview)){
-                    reviewPairs.add(new ReviewPair(fstReview, sndReview));
-                }
-            }
-        }
-        return Math.sqrt(reviewPairs.stream().mapToDouble(reviewPair -> Math.pow(reviewPair.fstReview.getScore()-reviewPair.sndReview.getScore(),2)).sum());
+      
+        final Collection<ReviewPair> sharedReviews = sharedReviews(reviewsFstPerson, reviewsSndPerson);
+        
+        final double sumOfSimilarities = sharedReviews.stream().mapToDouble(reviewPair -> Math.pow(reviewPair.fstReview.getScore()-reviewPair.sndReview.getScore(),2)).sum();
+        
+        return Math.sqrt(sumOfSimilarities);
+        
     }
     private class ReviewPair{
         Review fstReview;
